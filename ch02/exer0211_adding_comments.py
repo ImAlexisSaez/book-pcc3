f9cc5e0a1f202e76af4059a564dab5809feebd8b
Alexis <cucoalexis@hotmail.com>
2022-12-18 20:44:50 +0100

    Añade ejemplos y ejercicios del capítulo 2, completo

---
+++ ch02/exer0211_adding_comments.py
@@ -0,0 +1,17 @@
+# Ejemplo de uso de variables y operaciones aritméticas con comentarios
+
+# Datos
+dato_1 = 10
+dato_2 = 15
+
+# Operaciones
+operacion_1 = dato_1 + dato_2
+operacion_2 = dato_1 - dato_2
+operacion_3 = dato_1 * dato_2
+operacion_4 = dato_1 / dato_2
+
+# Muestra el resultado de la operación por pantalla
+print(operacion_1)
+print(operacion_2)
+print(operacion_3)
+print(operacion_4)
