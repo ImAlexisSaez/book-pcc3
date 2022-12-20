6e055b39b1f2af72c9a89b9cfdc1249bcf0d918d
Alexis <cucoalexis@hotmail.com>
2022-12-20 18:28:07 +0100

    Añade ejemplos y ejercicios del capítulo 3, completo

---
+++ ch03/example0304_motorcycles.py
@@ -0,0 +1,8 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+#print(motorcycles[3]) # IndexError
+
+print(motorcycles[-1])
+
+motorcycles_2 = []
+#print(motorcycles_2[-1]) # IndexError
---
+++ ch03/exer0311_intentional_error.py
@@ -0,0 +1,6 @@
+lista = ['patatas', 'guisantes', 'agua']
+
+print(lista)
+
+#print(lista[3]) # IndexError
+print(lista[-1])
