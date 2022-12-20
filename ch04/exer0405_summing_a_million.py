5a5ef6894a8fffb76d8bdb98d5da43dfc902042f
Alexis <cucoalexis@hotmail.com>
2022-12-20 22:00:51 +0100

    Bucles con números

---
+++ ch04/exer0405_summing_a_million.py
@@ -0,0 +1,5 @@
+numeros = list(range(1,1000001))
+
+print(f'El valor mínimo de la lista es:\t\t {min(numeros)}.')
+print(f'El valor máximo de la lista es:\t\t {max(numeros)}.')
+print(f'La suma de todos ellos asciende a:\t {sum(numeros)}.')
