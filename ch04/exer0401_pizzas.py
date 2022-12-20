6e138530bd61676efb0f866b8ca5a5a489fa2e1a
Alexis <cucoalexis@hotmail.com>
2022-12-20 18:47:07 +0100

    Añade ejemplos y ejercicios del capítulo 4, primeros bucles

---
+++ ch04/exer0401_pizzas.py
@@ -0,0 +1,7 @@
+pizzas = ['Carbonara', 'Hawaiana', 'Americana']
+
+for pizza in pizzas:
+    #print(pizza)
+    print(f'Me gustaría cenar una pizza {pizza}.')
+
+print(f'\nSí, tengo antojo de pizza esta noche.')
---
+++ ch04/exer0402_animals.py
@@ -0,0 +1,7 @@
+animales = ['perro', 'gato', 'pez']
+
+for animal in animales:
+    #print(animal)
+    print(f'¡Un {animal} es una excelente mascota!')
+
+print(f'\nCualquiera de ellos sería una estupenda mascota.')
