b1eb179584468854a12db0ebb2a1e3286f4d62d1
Alexis <cucoalexis@hotmail.com>
2022-12-30 23:00:48 +0100

    Clase que contiene los ajustes del juego.

---
+++ ch12/ajustes.py
@@ -0,0 +1,8 @@
+class Ajustes:
+    """Recoge los ajustes del juego ¡Invasión alienígena!"""
+
+    def __init__(self):
+        """Inicializa los ajustes del juego."""
+        self.pantalla_ancho = 1200
+        self.pantalla_largo = 800
+        self.color_fondo = (230, 230, 230)
