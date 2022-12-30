6738e56ec0f5ceea1f3dba00eaeeebd76ce29ed9
Alexis <cucoalexis@hotmail.com>
2022-12-30 23:22:33 +0100

    Clase que gestiona la nave del jugador.

---
+++ ch12/cohete.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Cohete:
+    """Gestiona la nave del jugador."""
+
+    def __init__(self, juego_ia):
+        """Inicializa la nave y establece su posición inicial."""
+        self.pantalla = juego_ia.pantalla
+        self.pantalla_rect = juego_ia.pantalla.get_rect()
+
+        # Carga la imagen de la nave y coge su rectángulo
+        self.imagen = pygame.image.load('ch12/images/ship.bmp')
+        self.rect = self.imagen.get_rect()
+
+        # Comienza cada nueva imagen en el centro de la parte 
+        # inferior de la pantalla
+        self.rect.midbottom = self.pantalla_rect.midbottom
+    
+    def dibuja_nave(self):
+        """Dibuja la nave en su posición actual."""
+        self.pantalla.blit(self.imagen, self.rect)
