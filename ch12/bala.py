8e4f92501afa5d3bb57533b84d3e226cc70cb6a7
Alexis <cucoalexis@hotmail.com>
2023-01-01 22:35:15 +0100

    Completa la clase bala.

--- ch12/bala.py
+++ ch12/bala.py
@@ -12,10 +12,22 @@ class Bala(Sprite):
         self.color = self.ajustes.color_bala
 
         # Crea un rectángulo en (0,0) y luego ajuste la posición
-        self.rect = pygame.rect(
+        self.rect = pygame.Rect(
             0, 0, self.ajustes.anchura_bala, self.ajustes.altura_bala
         )
         self.rect.midtop = ia_juego.cohete.rect.midtop
 
         # Almacena la posición como float
         self.y = float(self.rect.y)
+    
+    def actualiza(self):
+        """Mueve la bala hacia arriba en la pantalla."""
+        # Actualiza la posición exacta de la bala
+        self.y -= self.ajustes.velocidad_bala
+
+        # Actualiza la posición del rectángulo
+        self.rect.y = self.y
+
+    def dibuja_bala(self):
+        """Dibuja la bala en la pantalla."""
+        pygame.draw.rect(self.pantalla, self.color, self.rect)
