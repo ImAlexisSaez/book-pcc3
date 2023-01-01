7b837b6da5881e30afcdc2b5611a7b2d4a7b1139
Alexis <cucoalexis@hotmail.com>
2023-01-01 23:02:33 +0100

    Implementa el disparo de balas.

--- ch12/invasion_alien.py
+++ ch12/invasion_alien.py
@@ -4,6 +4,7 @@ import pygame
 
 from ajustes import Ajustes
 from cohete import Cohete
+from bala import Bala
 
 class InvasionAlien:
     """Clase que gestiona los activos del juego y su comportamiento."""
@@ -21,12 +22,14 @@ class InvasionAlien:
         pygame.display.set_caption("¡Invasión de alienígenas!")
 
         self.cohete = Cohete(self)
+        self.balas = pygame.sprite.Group()
 
     def ejecuta_juego(self):
         """Inicia el bucle principal para el juego."""
         while True:
             self._controla_eventos()  
-            self.cohete.actualiza_posicion()          
+            self.cohete.actualiza_posicion() 
+            self.balas.update()
             self._actualiza_pantalla()
             self.reloj.tick(60)
     
@@ -46,6 +49,8 @@ class InvasionAlien:
             self.cohete.moviendo_derecha = True
         elif event.key == pygame.K_LEFT:
             self.cohete.moviendo_izquierda = True
+        elif event.key == pygame.K_SPACE:
+            self._dispara_bala()
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -56,9 +61,16 @@ class InvasionAlien:
         elif event.key == pygame.K_LEFT:
             self.cohete.moviendo_izquierda = False
     
+    def _dispara_bala(self):
+        """Crea una nueva bala y la añade al grupo de balas."""
+        nueva_bala = Bala(self)
+        self.balas.add(nueva_bala)
+    
     def _actualiza_pantalla(self):
         """Actualiza las imágenes en la pantalla y las dibuja (flip)."""
         self.pantalla.fill(self.ajustes.color_fondo)
+        for bala in self.balas.sprites():
+            bala.dibuja_bala()
         self.cohete.dibuja_nave()
         
         pygame.display.flip()
